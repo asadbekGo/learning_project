95d7cafc959a4fb3ac1de6c492cb39bf1ba92ec7
Asadbek <asadbek30665@gmail.com>
2023-09-29 17:07:08 +0500

    add calculate package

---
+++ calculate/sub.go
@@ -0,0 +1,5 @@
+package calculate
+
+func Sub(a, b int) int {
+	return a - b
+}
---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"app/calculate"
+)
+
+func main() {
+
+	fmt.Println(calculate.Add(10, 20))
+	fmt.Println(calculate.Sub(10, 20))
+}
